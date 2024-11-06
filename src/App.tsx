2c1de744b5f784002d47855bba7d70965f56d4ee
Sergio <norgestream@gmail.com>
2024-11-06 19:24:08 +0200

    create reusable Button component with dynamic types

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Input from './components/input';
+import Button from './components/Button';
+
 function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <main>
+      <Input id="name" label="Your name" type="text" />
+      <Input id="age" label="Your age" type="number" />
+      <p>
+        <Button>A Button</Button>
+      </p>
+      <p>
+        <Button href='https://google.com'>A Link</Button>
+      </p>
+    </main>
+  );
 }
 
 export default App;
