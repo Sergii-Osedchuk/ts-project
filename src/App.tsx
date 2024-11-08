61f493550665cf6dc569862236355f3cb31d957d
Sergio <norgestream@gmail.com>
2024-11-08 21:39:36 +0200

    use forwardRef with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Input from './components/input';
 import Button from './components/Button';
 import Container from './components/Container';
+import { useRef } from 'react';
 
 function App() {
+  const input = useRef<HTMLInputElement>(null);
   return (
     <main>
       <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" />
+      <Input id="age" label="Your age" type="number" ref={input} />
       <p>
         <Button>A Button</Button>
       </p>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,20 @@
-import { ComponentPropsWithoutRef, type FC } from "react";
+import { ComponentPropsWithoutRef, forwardRef } from "react";
 
 type InputProps = {
   label: string;
   id: string;
 } & ComponentPropsWithoutRef<'input'>;
 
-const Input: FC<InputProps> = ({ label, id, ...props }) => {
+const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
+  { label, id, ...props },
+  ref
+) {
   return (
     <p>
       <label htmlFor={id}>{label}</label>
-      <input id={id} {...props} />
+      <input id={id} {...props} ref={ref} />
     </p>
-
   )
-}
+})
 
 export default Input;
