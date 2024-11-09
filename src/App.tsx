832a8b7133a77602ec238241789aa7e343c6343b
Sergio <norgestream@gmail.com>
2024-11-09 22:23:37 +0200

    used forwardRef and useImperetiveHandle for forwarding ref with ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
-import Input from './components/input';
+import Input from './components/Input';
 import Button from './components/Button';
 import { useRef } from 'react';
 import Form from './components/Form';
+import { type FormHandle } from './components/Form';
 
 function App() {
+  const customForm = useRef<FormHandle>(null);
+
   const input = useRef<HTMLInputElement>(null);
   function handleSave(data: unknown) {
     const extractedData = data as { name: string; age: string };
-    console.log(extractedData)
+    console.log(extractedData);
+    customForm.current?.clear()
   }
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input id="name" label="Your name" type="text" />
         <Input id="age" label="Your age" type="number" ref={input} />
         <Button>Save</Button>
       </Form>
-      {/* <p> */}
-      {/* </p>
-      <p>
-        <Button href='https://google.com'>A Link</Button>
-      </p>
-      <Container as={Button} onClick={() => { }}>Click me</Container> */}
     </main>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,24 @@
-import { ComponentPropsWithoutRef, FormEvent } from "react"
+import { ComponentPropsWithoutRef, FormEvent, useRef, forwardRef, useImperativeHandle } from "react"
 
 type FormProps = ComponentPropsWithoutRef<'form'> & {
   onSave: (value: unknown) => void;
 }
 
-export default function Form({onSave, children, ...otherProps}: FormProps) {
+export type FormHandle = {
+  clear: () => void;
+}
+
+const Form =  forwardRef<FormHandle, FormProps>(function Form({onSave, children, ...otherProps}, ref) {
+  const form = useRef<HTMLFormElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      clear() {
+        form.current?.reset();
+      }
+    }
+  })
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
@@ -13,8 +27,10 @@ export default function Form({onSave, children, ...otherProps}: FormProps) {
     onSave(data);
   }
   return (
-    <form onSubmit={handleSubmit} {...otherProps}>
+    <form onSubmit={handleSubmit} {...otherProps} ref={form}>
       {children}
     </form>
   )
-}
+})
+
+export default Form;
